99f9b11e6f049fa969f1dcd2f1eb1dbac2b10fa4
headjuancho <juan.arciniegasm@upb.edu.co>
2022-03-12 16:10:19 -0500

    programadas clase Jugador y Atacable
    metodo Atacar() y clase Atacable definidas, constructores y accesores apropiados.

--- ProyectoFinalScripting/Atacable.cs
+++ ProyectoFinalScripting/Atacable.cs
@@ -8,5 +8,24 @@ namespace ProyectoFinalScripting
 {
     class Atacable
     {
+        
+        internal uint poder;
+        internal bool esObstaculo;
+
+        public Atacable(bool esObstaculo, uint poder)
+        {
+            this.esObstaculo = esObstaculo;
+            Poder = poder;
+        }
+
+        public uint Poder { get => poder; set
+
+            {
+                if (value > 0)
+                    poder = value;
+                else
+                    throw new Exception("el poder debe ser mayor a cero");
+            }
+        }
     }
 }
--- ProyectoFinalScripting/Jugador.cs
+++ ProyectoFinalScripting/Jugador.cs
@@ -8,5 +8,82 @@ namespace ProyectoFinalScripting
 {
     class Jugador
     {
+        internal uint poder;
+        string nombre;
+        internal byte vidas;
+
+        public Jugador(uint poder, string nombre)
+        {
+            Poder = poder;
+            Nombre = nombre;
+            vidas = 3;
+        }
+
+        public uint Poder {
+            get => poder;
+
+            set
+            {
+                if (value > 0)
+                {
+                    poder = value;
+                }
+
+                else
+                    throw new Exception("el poder inicial del usuario no puede ser cero");
+            }
+        }
+       
+
+        public string Nombre
+        {
+            get => nombre; set
+            {
+
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("el nombre no puede tener espacios en blanco  ");
+                }
+                else
+                {
+                    nombre = value;
+                }
+
+            }
+        }
+
+        public bool Atacar(Atacable target)
+        {
+            bool victoria = false;
+
+            if (target.esObstaculo)
+            {
+                Poder += target.poder;
+                victoria = true;
+                return victoria;
+            }
+
+            else
+            {
+                
+                if(target.poder >= Poder)
+                {
+                    victoria = false;
+                    vidas--;
+                    return victoria;
+                }
+
+                else
+                {
+                    Poder += target.Poder;
+                    return victoria;
+                }
+               
+            }
+
+        }
     }
+
+    
+
 }
