c74a0537b3a4a7861dc1c04ca762b2a677e7f871
headjuancho <juan.arciniegasm@upb.edu.co>
2022-03-13 00:26:28 -0500

    Update TorreEnemigo.cs
    cambio la clase

--- ProyectoFinalScripting/TorreEnemigo.cs
+++ ProyectoFinalScripting/TorreEnemigo.cs
@@ -7,15 +7,26 @@ using System.Threading.Tasks;
 namespace ProyectoFinalScripting
 {
     class TorreEnemigo : Torre
+
     {
-        public TorreEnemigo(uint altura) : base(altura)
+        Atacable atacable;
+        internal List<Atacable> l_atacable;
+        public TorreEnemigo(uint altura, Atacable atacable) : base(altura)
         {
+
+            this.Altura = altura;
+            this.atacable = atacable;
+            l_atacable = new List<Atacable>((int)altura);
+            l_atacable.Add(atacable);
+            
         }
 
-       internal static void ReducirAltura()
+       internal void ReducirAltura(Atacable target)
        {
-
+            l_atacable.Remove(target);
+            Altura--;
        }
+
         static void LlenarTorre()
         {
 
